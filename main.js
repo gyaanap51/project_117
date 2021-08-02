f51504c76210fbdebc5b6e189032344074c29fd3
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-08-02 12:30:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(640, 480)
     video.hide();
+    classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KxtS-IzSN/model.json',modelLoaded);
 }
 function draw(){
     image(video, 0, 0, 640, 480);
+    classifer.classify(video, gotResult);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy_name").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
