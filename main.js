45efc5d353d6a09ea83c823d99ad0015835005ca
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-10-02 14:28:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
-function setup(){
-    canvas = createCanvas(648, 480);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(640, 480)
-    video.hide();
-    classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KxtS-IzSN/model.json',modelLoaded);
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;  
+rightWristX = 0;
+rightWristY = 0;
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
 }
-function draw(){
-    image(video, 0, 0, 640, 480);
-    classifer.classify(video, gotResult);
+
+function setup()
+{
+    canvas = createCanvas(640,480);
+    canvas.position(650,200);
+    video = createCapture(VIDEO);
+
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);  
+    poseNet.on('pose', gotPoses); 
+
+    
+    
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX +"leftWristY ="+ leftWristY);
+
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX +"rightWristY ="+ rightWristY);
+    }
 }
 
 function modelLoaded(){
-    console.log('Model Loaded!');
+    console.log('PoseNet Is Initialized');
 }
 
-function gotResult(error, results) {
-    if(error) {
-        console.error(error);
-    } else {
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_accuracy_name").innerHTML = results[0].confidence.toFixed(3);
-    }
+function draw(){
+    image(video, 0, 0, 640, 480);
+}
+function play_Harry(){
+    song1.play();
+}
+function play_Peter(){
+    song2.play();
+}
+function pause(){
+    song2.pause();
+    song1.pause()
 }
