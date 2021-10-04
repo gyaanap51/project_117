75faa630f7e5cc0783896930ca241f41afbb599b
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-10-04 14:38:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX = 0;
 leftWristY = 0;  
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+
 function preload()
 {
     song1 = loadSound("music.mp3");
@@ -30,6 +32,7 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;        
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX +"leftWristY ="+ leftWristY);
@@ -47,6 +50,13 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 640, 480);
+    fill("#ff0000");
+    stroke("#ff0000");
+    if(scoreLeftWrist > 0.2 ){
+        song1.play();
+    }
+    circle(leftWristX,leftWristY,20);
+
 }
 function play_Harry(){
     song1.play();
